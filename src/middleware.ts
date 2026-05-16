8fa0db16332da6176296756f6d7d109f87893f99
CastDev-j <23031429@itcelaya.edu.mx>
2026-05-15 20:00:49 -0600

    cloudflare integrated, Header component moved from component/shared to component/shared/header

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { getActionContext } from "astro:actions";
+import { env } from "cloudflare:workers";
+import { defineMiddleware, sequence } from "astro:middleware";
+
+const rateLimit = defineMiddleware(async (context, next) => {
+  const { success } = await env.RATE_LIMIT.limit({
+    key: context.url.pathname,
+  });
+
+  if (!success) {
+    return new Response("Too many requests", { status: 429 });
+  }
+
+  return next();
+});
+
+export const onRequest = sequence(rateLimit);
